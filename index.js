04952007275472605f944eb67cba4dbf68d24d3e
thakur8698589823 <76060611+thakur8698589823@users.noreply.github.com>
2022-05-04 10:59:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// const herosection = document.querySelector(".section-hero")
+
+// const footerElem = document.querySelector(".section-footer")
+
+// const scrollElement = document.createElement("div");
+
+// scrollElement = document.createElement("scrollTop-style")
+
+// scrollElement.innerHTML=`<ion-icon name="arrow-up-outline" class="scroll-top"></ion-icon> `
+
+// footerElem.after(scrollElement);
+
+
+
+
+// const scrollElement = document.querySelector(".scrollTop-style");
+// console.log(scrollElement)
+
+// const scrollTop =() =>{
+//     herosection.scrollIntoView({behavior: "smooth"});
+// };
+
+// scrollElement.addEventListener("click", scrollTop)
+
+
+// let scrollTop = document.querySelector(".scrollTop-style");
+// console.log(scrollTop);
+// scrollTop.onclick = function(){
+//     window.scrollTo({behavior: 'smooth',top:0})
+// }
